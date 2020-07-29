fb2bfe149f892d981f3bf9b2f58518c76e83c79c
Ayush Deotare <ayush2deotare@gmail.com>
2020-07-30 03:19:10 +0530

    Update Main.java
    Modified channel name.

--- GlobalExecute[Spigot]/src/me/ayush_03/globalexecute/Main.java
+++ GlobalExecute[Spigot]/src/me/ayush_03/globalexecute/Main.java
@@ -1,38 +1,34 @@
 package me.ayush_03.globalexecute;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
-public class Main extends JavaPlugin implements PluginMessageListener {
-	
-	public void onEnable() {
-		// Registering the channel..
-		 Bukkit.getMessenger().registerIncomingPluginChannel(this, "Return", this);
-	}
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
 
-	@Override
-	public void onPluginMessageReceived(String channel, Player p, byte[] message) {
-		
-		DataInputStream in = new DataInputStream(new ByteArrayInputStream(message));
-		
-		try {
-			String sub = in.readUTF();
-			if (sub.equals("command")) {
-				
-				// Signal received.
-				
-				String cmd = in.readUTF();
-				System.out.println("[GlobalExecute] Received a command message from BungeeCord, executing it.");
-				getServer().dispatchCommand(getServer().getConsoleSender(), cmd); 
-				
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+public class Main extends JavaPlugin implements PluginMessageListener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getMessenger().registerIncomingPluginChannel(this, "globalexecute:channel", this);
+    }
+
+    @Override
+    public void onPluginMessageReceived(String channel, Player p, byte[] message) {
+        DataInputStream in = new DataInputStream(new ByteArrayInputStream(message));
+        try {
+            String subChannel = in.readUTF();
+
+            if (subChannel.equalsIgnoreCase("command")) {
+                String command = in.readUTF();
+                System.out.println("[GlobalExecute] Received a command request from BungeeCord, executing it.");
+                getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
