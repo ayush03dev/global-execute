3abce9d166c73c755f8fb8333fde82c310f1a9e0
Ayush Deotare <ayush2deotare@gmail.com>
2020-07-30 03:18:29 +0530

    Update Main.java
    Modified channel name.

--- GlobalExecute[Bungee]/src/me/ayush_03/globalexecute/Main.java
+++ GlobalExecute[Bungee]/src/me/ayush_03/globalexecute/Main.java
@@ -4,9 +4,9 @@ import net.md_5.bungee.BungeeCord;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class Main extends Plugin {
-	
-	public void onEnable() {
-		BungeeCord.getInstance().getPluginManager().registerCommand(this, new CommandClass());
-		BungeeCord.getInstance().registerChannel("Return");
-	}
+
+    public void onEnable() {
+        BungeeCord.getInstance().getPluginManager().registerCommand(this, new CommandClass());
+        BungeeCord.getInstance().registerChannel("globalexecute:channel");
+    }
 }
