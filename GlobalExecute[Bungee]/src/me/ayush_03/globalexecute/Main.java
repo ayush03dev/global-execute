0166681957a7e141543412a5c5952f0250bab37a
Ayush <ayush2deotare@gmail.com>
2021-01-30 17:25:15 +0530

    Exploit Fix

--- GlobalExecute[Bungee]/src/me/ayush_03/globalexecute/Main.java
+++ GlobalExecute[Bungee]/src/me/ayush_03/globalexecute/Main.java
@@ -5,18 +5,37 @@ import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.Map;
 
 public class Main extends Plugin implements Listener {
 
-    public static final String channel = "gexecute:channel";
+    // Already initialized as the default channel name
+    public static String channel = "ge:cmdchannel";
+
 
     public void onEnable() {
+        saveDefaultConfig();
         BungeeCord.getInstance().getPluginManager().registerCommand(this, new CommandClass());
         BungeeCord.getInstance().getPluginManager().registerListener(this, this);
+
+        try {
+            Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+            if (configuration.getString("channel") != null) {
+                String channelName = configuration.getString("channel");
+                if (channelName.contains(":")) {
+                    channel = channelName;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         BungeeCord.getInstance().registerChannel(channel);
     }
 
@@ -62,5 +81,21 @@ public class Main extends Plugin implements Listener {
             e.printStackTrace();
         }
         server.sendData(channel, stream.toByteArray());
+    }
+
+    private void saveDefaultConfig() {
+        if (!getDataFolder().exists())
+            getDataFolder().mkdir();
+
+        File file = new File(getDataFolder(), "config.yml");
+
+
+        if (!file.exists()) {
+            try (InputStream in = getResourceAsStream("config.yml")) {
+                Files.copy(in, file.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
